e2fc42a71d3d4269999146f3be3f9a9ca1235a58
Tan Choon Yong <tanchoonyong8@gmail.com>
2025-08-24 19:30:13 +0800

    Optimise the training loop by calling tensor.item() every epoch instead of every batch

--- Code/trainModel.py
+++ Code/trainModel.py
@@ -34,18 +34,19 @@ def trainModel(model:nn.Module, train_loader,optimiser=None,device=torch.device(
     
     for epoch in range(num_epochs):
         model.train()
-        running_loss = 0
+        losses = torch.tensor(0.0, device=device)
         for images, mol, labels in train_loader:
             images = images.to(device)
             mol = mol.to(device)
             labels = labels.to(device)
             optimiser.zero_grad()
             logits = model(images,mol)
-            loss = loss_fn(logits,labels)
+            loss:torch.Tensor = loss_fn(logits,labels)
             loss.backward()
             optimiser.step()
-            running_loss += loss.item()
-        print(f"Epoch {epoch} Done. Avg Loss: {running_loss / len(train_loader):.4f}")
+            losses+=loss.detach()
+        avg_loss = (losses/len(train_loader)).item()
+        print(f"Epoch {epoch} Done. Avg Loss: {avg_loss:.4f}")
         
         if val_loader is not None:
             roc_auc = evaluate_roc_auc(model,val_loader)
