7c7a916e1661734326c7e001359579e1b11c464f
Tan Choon Yong <tanchoonyong8@gmail.com>
2025-10-26 21:49:44 +0800

    Trying out crossAttentionFusion

---
+++ Code/models/ResNet/model_with_crossAttentionFusionAndDropout.py
@@ -0,0 +1,119 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from typing import Literal
+from models.crossAttentionFusion import crossAttentionFusion
+
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels,stride):
+        super().__init__()
+        
+        self.bn1 = nn.BatchNorm3d(in_channels)
+        self.conv1 = nn.Conv3d(in_channels, out_channels, 
+                               kernel_size=3, stride=stride,padding=1,bias=False)
+        
+        self.bn2 = nn.BatchNorm3d(out_channels)
+        self.conv2 = nn.Conv3d(out_channels, out_channels, 
+                               kernel_size=3, stride=1,padding=1,bias=False)
+
+        if (stride != 1) or (in_channels != out_channels):
+            self.downsample = nn.Sequential(
+                nn.Conv3d(in_channels, out_channels, 
+                          kernel_size=1, stride=stride,bias=False),
+                nn.BatchNorm3d(out_channels)
+            )
+        else: self.downsample = None
+
+    
+    def forward(self, x):
+        identity = x
+        
+        out = self.bn1(x)  
+        out = F.relu(out)
+        out = self.conv1(out)
+
+        
+        out = self.bn2(out)  
+        out = F.relu(out)
+        out = self.conv2(out)
+        
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        
+        out += identity
+        out = F.relu(out)
+        return out
+    
+class ResNet(nn.Module):
+    def __init__(self,in_channels,):
+        super().__init__()
+        self.conv1 = nn.Conv3d(in_channels, 16, kernel_size=7, stride=(1,2,2), padding=3,bias=False)
+        self.bn1 = nn.BatchNorm3d(16)
+        self.maxpool = nn.MaxPool3d(kernel_size=3, stride=(1,2,2), padding=1)
+        self.layer1 = nn.Sequential(
+            ResBlock(16,16,stride=1),
+            ResBlock(16,16,stride=1)
+        )
+        self.layer2 = nn.Sequential(
+            ResBlock(16,32,stride=(1,2,2)),
+            ResBlock(32,32,stride=1)
+        )
+        self.layer3 = nn.Sequential(
+            ResBlock(32,64,stride=(1,2,2)),
+            ResBlock(64,64,stride=1)
+        )
+        self.layer4 = nn.Sequential(
+            ResBlock(64,128,stride=(1,2,2)),
+            ResBlock(128,128,stride=1)
+        )
+        self.avgpool = nn.AdaptiveAvgPool3d((1,1,1))
+        self.fc = nn.Linear(128,128)
+        self.bn2 = nn.BatchNorm1d(128)
+    def forward(self,x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x,1)
+        x = F.relu(self.fc(x))
+        x = self.bn2(x)
+        return x
+
+class Model(nn.Module):
+    def __init__(self,hidden_dim,feedforward_dim,dropout):
+        super().__init__()
+        self.preNac = ResNet(6)
+        self.postNac = ResNet(6)
+        self.dropout = nn.Dropout()
+        self.fusion = crossAttentionFusion(hidden_dim,feedforward_dim,dropout)
+        self.mols_encoder = nn.Sequential(
+            nn.Linear(2,32),
+            nn.ReLU(),
+            nn.BatchNorm1d(32)
+        )
+        self.fc1 = nn.Linear(hidden_dim+32,64)
+        self.ln = nn.LayerNorm(64)
+        self.fc2 = nn.Linear(64,1)
+    def forward(self,preNacVol,postNacVol,mols,mode:Literal["preNac","both"]):
+        preNacEmbed = self.preNac(preNacVol)
+        if mode=="preNac":
+            postNacEmbed = torch.zeros_like(preNacEmbed)
+        elif mode=="both":
+            postNacEmbed = self.dropout(self.postNac(postNacVol))
+        
+        fused = self.fusion(preNacEmbed,postNacEmbed)
+        
+        molsEmbed = self.mols_encoder(mols)
+        x = torch.cat([fused,molsEmbed],dim=1)
+        x = F.gelu(self.fc1(x))
+        x = self.ln(x)
+        x = self.fc2(x)
+        return x
+        
+
+        
---
+++ Code/models/crossAttentionFusion.py
@@ -0,0 +1,65 @@
+import torch.nn.functional as F
+from torch import nn
+import torch,math
+
+class FeedForward(nn.Module):
+    def __init__(self,hidden_dim,feedforward_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(hidden_dim,feedforward_dim)
+        self.fc2 = nn.Linear(feedforward_dim,hidden_dim)
+    def forward(self,x):
+        x = F.gelu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+class crossAttentionFusion(nn.Module):
+    def __init__(self,hidden_dim,feedforward_dim,dropout):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+        self.dropout = dropout
+        self.ln1 = nn.LayerNorm(hidden_dim)
+        self.ln2 = nn.LayerNorm(hidden_dim)
+        self.q_proj = nn.Linear(hidden_dim,hidden_dim)
+        self.kv_proj = nn.Linear(hidden_dim,2*hidden_dim)
+        self.ln3 = nn.LayerNorm(hidden_dim)
+        self.ff = FeedForward(hidden_dim,feedforward_dim)
+        self.dropout = nn.Dropout(0.3)
+        self.ln4 = nn.LayerNorm(hidden_dim)
+        
+    def forward(self,preNacEmbed,postNacEmbed):
+        
+        assert preNacEmbed.shape == postNacEmbed.shape
+        
+        preNacEmbed = self.ln1(preNacEmbed)
+        postNacEmbed = self.ln2(postNacEmbed)
+        
+        query = self.q_proj(preNacEmbed)
+        kv = self.kv_proj(postNacEmbed) # (B,hidden_dim) -> (B,2*hidden_dim)
+        
+        key,value = kv.chunk(2,dim=-1) # (B,2*hidden_dim) -> (B,hidden_dim), (B,hidden_dim)
+        attentionWeights = F.softmax((query*key).sum(dim=-1,keepdim=True)/math.sqrt(self.hidden_dim),dim=-1)
+        
+        attentionOutput = attentionWeights*value
+        
+        if self.dropout:
+            attentionOutput = self.dropout(attentionOutput)
+        
+        x = attentionOutput + preNacEmbed
+        
+        x = self.ln3(x)
+        
+        ff_out = self.ff(x)
+        
+        x = self.dropout(ff_out) + x
+        
+        x = self.ln4(x)
+        
+        return x
+    
+
+
+
+
+
+        
+        
